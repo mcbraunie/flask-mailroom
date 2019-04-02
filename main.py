bbac387f53c52776af16e9a8999944404c223f1f
Micah <mcbraun@uw.edu>
2019-04-02 09:10:36 -0700

    initial

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('donations.jinja2')
+    elif request.method == 'POST':
+        donor = Donor.select().where(Donor.name == request.form['name']).get()
+        donation = Donation(donor=donor, value=float(request.form['value']))
+        donation.save()
+        
+        return redirect(url_for('home'))
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
